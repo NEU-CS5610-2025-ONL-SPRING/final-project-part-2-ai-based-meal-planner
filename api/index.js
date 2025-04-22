6a14d637b002dd643080ae25a37ddec972d9a9b8
xiangyingsun <xysun325@gmail.com>
2025-04-21 19:20:58 -0700

    part 3 deploy

--- api/index.js
+++ api/index.js
@@ -1,16 +1,15 @@
 require('dotenv').config();
 
-const PORT = parseInt(process.env.PORT) || 8080;
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT} 🚀`);
-});
 
+const app = express();
+
+const PORT = parseInt(process.env.PORT) || 8080;
 
 
 const express = require('express');
 const cookieParser = require('cookie-parser');
 const routes = require('./routes');
-const app = express();
+
 const mealplanRoutes = require('./routes/mealplans');
 const mealIdeasRoute = require('./routes/mealIdeas');
 
@@ -22,4 +21,8 @@ app.use('/api', routes);
 app.use('/api/mealplans', mealplanRoutes);
 app.use('/api/meal-ideas', mealIdeasRoute);
 
-app.listen(3001, () => console.log('Server running on port 3001'));
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT} 🚀`);
+  });
+
+//app.listen(3001, () => console.log('Server running on port 3001'));
