a934344b3643dfd2ae05596827ae986f5cbeb68e
xiangyingsun <xysun325@gmail.com>
2025-04-21 23:06:22 -0700

    config

--- client/src/config.js
+++ client/src/config.js
@@ -5,4 +5,4 @@
 // if (!import.meta.env.VITE_API_URL) {
 //     throw new Error('Missing VITE_API_URL in environment variables');
 //   }
-export const API_BASE = "https://final-project-part-2-ai-based-meal.onrender.com/";
+export const API_BASE = "https://final-project-part-2-ai-based-meal.onrender.com";
