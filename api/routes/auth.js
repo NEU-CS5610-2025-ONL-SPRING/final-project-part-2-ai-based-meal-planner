18c4f3767c0acfe49231640db0d43e26b0b770ff
xiangyingsun <xysun325@gmail.com>
2025-04-21 23:28:10 -0700

    Update

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -38,7 +38,7 @@ router.post('/login', async (req, res) => {
 //    });
   
 
-  res.cookie('token', token, { httpOnly: true, sameSite: 'None' });
+  res.cookie('token', token, { httpOnly: true, secure: true, sameSite: 'None' });
   res.json({ success: true });
 });
 
